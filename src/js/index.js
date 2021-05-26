ed0d3df71961a39927ffbff68fd3f8e2f4266676
story <hassn07809479901@gmail.com>
2021-05-26 18:24:02 +0300

    not SPA

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+document.querySelector(".navbar-burger").addEventListener("click", () =>
+document.querySelector("header").classList.toggle("is-active"));
+
+document.querySelector(".navbar-item").addEventListener("click",()=>
+this.add(".active"));
+
+ 
+
+
+
